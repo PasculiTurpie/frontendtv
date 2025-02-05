869795da431e5ec5982d2388de06d781912cdd57
Pasculi <rodrigo.s.turpie@gmail.com>
2025-02-05 10:02:32 -0300

    feat:Cambio a https

--- src/components/Consulta.js
+++ src/components/Consulta.js
@@ -1,5 +1,5 @@
 
-const result = frtch("http://api.tv-operaciones.cl/api/v1/channel")
+const result = fetch("http://api.tv-operaciones.cl/api/v1/channel")
   .then(response => response.json())
   .then(data => {
   console.log(data);
